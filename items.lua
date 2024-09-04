85616d13d81fa24ea1899a87478ea9ef4a726bdd
Edmondio <83596559+Edmondio@users.noreply.github.com>
2024-09-04 15:54:33 +0200

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,21 @@
+-- Add this to your items.lua for OX INVENTORY ONLY
+
+ ['scuba_set'] = {
+ 	label = 'Ensemble de plongée',
+ 	weight = 2000,
+ 	description = 'Équipement de plongée',
+ 	stack = false,
+ 	client = {
+ 		export = 'ed_scuba.wear'
+ 	}
+ },
+ ['scuba_fins'] = {
+ 	label = 'Palmes de plongée',
+ 	weight = 200,
+ 	description = 'Équipement de plongée, aide à la natation',
+ 	stack = false,
+ 	client = {
+ 		export = 'ed_scuba.wear'
+ 	}
+ },
+
